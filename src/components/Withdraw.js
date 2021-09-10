a0972fac789f31e8b4fec6493c2eb91268985981
yuuhub <yuuriayano@gmail.com>
2021-09-10 20:11:50 +0800

    Add initial Withdraw

---
+++ src/components/Withdraw.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {withdraw, search_username} from '../bank_functionalities/bank_functions'
+
+const Withdraw = () => {
+    handleSubmit = (e) => {
+        console.log("Submitted");
+        e.preventDefault();
+    }
+
+    return (
+        <div>
+            <h1>Withdraw</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Account No.</label>
+                <input type="text" />
+                <label>Amount</label>
+                <input type="number" />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    )
+}
+
+export default Withdraw
