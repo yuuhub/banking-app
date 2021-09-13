3091007113497ef86e1c2942db9d73cdbad8a358
yuuhub <yuuriayano@gmail.com>
2021-09-13 20:49:03 +0800

    Change accountNo format

--- src/components/AddUserForm.js
+++ src/components/AddUserForm.js
@@ -19,7 +19,10 @@ const AddUserForm = ({users, setUsers }) => {
     const handleChange = (e) => {
         const newUser = {
             ...user, 
-            accountNo: (localStorage.length + 1)
+            accountNo: (localStorage.length + 1).toLocaleString('en-US', {
+                minimumIntegerDigits: 5,
+                useGrouping: false
+              })
         };
         newUser[e.target.name.toLowerCase()] = e.target.value.toLowerCase();
         setUser(newUser);
