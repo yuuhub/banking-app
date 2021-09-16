ade595535b3e47823da5b215d5b20d4ac944fd8e
yuuhub <yuuriayano@gmail.com>
2021-09-17 06:05:52 +0800

    Add margin top to welcome message

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -17,6 +17,7 @@ const Dashboard = () => {
 
     return (
         <div>
+            <h1>Dashboard</h1>
             <div className='welcome-container'>
                 <div className='title-container'>
                     <h1>Welcome to the Dashboard</h1>
